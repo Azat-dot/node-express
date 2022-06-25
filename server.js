50320eadcd3f9c71c0a7e157dd7fbc8e1133a6cb
azat <azatsafaryan@mail.ru>
2022-06-25 11:13:55 +0500

    response

---
+++ server.js
@@ -0,0 +1,12 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    response.send("<h1> Hello, world! </h1>")
+});
+
+app.listen(3000, function(){
+    console.log("Server startedon port 3000 ");
+})
